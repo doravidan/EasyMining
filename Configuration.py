a36aafa9c4c247a0038ec3ada6fb3604a108bcdd
doravi <avidandor@gmail.com>
2017-08-18 15:57:52 +0300

    removed gui start on main use configuration file

--- Configuration.py
+++ Configuration.py
@@ -1,13 +1,20 @@
-__author__ = 'doravi'
-
-
+#Enter your wallets address 
 wallet_address_ETH = "0x89e566af36e7274f0ddd84ba87184e6f3aa2e82f"
-wallet_address_Decred = "0x89e566af36e7274f0ddd84ba87184e6f3aa2e82f"
+wallet_address_Decred = "DsSzobNmudnYjFJfosfr5DpyvQd4Cdgtz91"
 wallet_address_Sia = "054fdf37dd0d9874f399c210e7bde6a4d752cd114faa542fa5499fcce9bd05a19a8e422a58ff"
 wallet_address_Pascalcoin = ""
 
-currency = "ETH" #Only ETH and dual
-miner = "Claymore" #Only clamore
-pool = "Ethermine" # Only Ethermine
+#Only ETH and dual
+currency = "ETH"
+
+#Only clamore
+miner = "Claymore"
+
+# ETH / Sia / Pascal =  nanopool, LBRY = suprnova, Decred = Yiimp
+pool = "Nanopool"
+
+#Enter suprniva login.worker
 suprnova_login_worker = "YourLogin.YourWorkerName"
-gpu = "1060" #choose gpu "470", "480", "570", "580", "1060", "1070", "1080", "1080ti"
+
+#Choose gpu "470", "480", "570", "580", "1060", "1070", "1080", "1080ti"
+gpu = "1070"
--- RunMiner.py
+++ RunMiner.py
@@ -13,9 +13,6 @@ def init_miner(config):
     #print("miner: " + config.miner)
     #print("pool: " + config.pool)
 
-    if config.pool == "ethermine":
-        config.pool = "eu1.ethermine.org:4444"
-
     cmd = command(config, first_result)
 
     # p = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -26,8 +23,6 @@ def init_miner(config):
 def run_miner_prof_check(cmd, first_result, config, p):
 
     result = check_proditability(config.gpu)
-
-    #result = check_proditability(config.gpu)
 
     time.sleep(3600)
     if result != first_result:
@@ -41,12 +36,12 @@ def command(config, result):
     cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eth-eu1.nanopool.org:9999', '-ewal', config.wallet_address_ETH, '-epsw', 'x']
 
     if result == "Pascalcoin":
-        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eu1.ethermine.org:4444', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://pasc-eu1.nanopool.org:15555', '-dwal', config.wallet_address_Pascalcoin, '-dpsw', 'x', '-dcoin', 'pasc']
+        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eth-eu1.nanopool.org:9999', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://pasc-eu1.nanopool.org:15555', '-dwal', config.wallet_address_Pascalcoin, '-dpsw', 'x', '-dcoin', 'pasc']
     elif result == "Decred":
-        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eu1.ethermine.org:4444', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://yiimp.ccminer.org:3252', '-dwal', config.wallet_address_Decred, '-dpsw', 'x']
+        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eth-eu1.nanopool.org:9999', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://yiimp.ccminer.org:3252', '-dwal', config.wallet_address_Decred, '-dpsw', 'x']
     elif result == "LBRY":
-        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eu1.ethermine.org:4444', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://lbry.suprnova.cc:6256', '-dwal', config.suprnova_login_worker, '-dpsw', 'x', '-dcoin', 'lbc']
+        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eth-eu1.nanopool.org:9999', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://lbry.suprnova.cc:6256', '-dwal', config.suprnova_login_worker, '-dpsw', 'x', '-dcoin', 'lbc']
     elif result == "Sia":
-        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eu1.ethermine.org:4444', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://stratum+tcp://eu.siamining.com:7777', '-dwal', config.wallet_address_Sia, '-dpsw', 'x', '-dcoin', 'sia']
+        cmd = ['Claymore\\ethdcrminer64.exe', '-epool', 'eth-eu1.nanopool.org:9999', '-ewal', config.wallet_address_ETH, '-epsw', 'x', '-dpool', 'stratum+tcp://sia-eu1.nanopool.org:7777', '-dwal', config.wallet_address_Sia, '-dpsw', 'x', '-dcoin', 'sia']
 
     return cmd
